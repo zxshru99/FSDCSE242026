344a7dbe28a56826f663b3b791f4a1e879d3a56d
zxshru99 <samridhc4@gmail.com>
2026-08-25 09:36:22 +0530

    New commit on 25-08-2026

--- resume.js
+++ resume.js
@@ -1,22 +1,23 @@
-const container=document.getElementById('container');
-const button =document.getElementById('btn');
+
+const loader = document.createElement('div');
+const container = document.getElementById('container');
+const button = document.getElementById('btn');
+
+
 container.appendChild(loader);
-const h1=document.createElement('h1');
-console.log(h1)
-h1.innerText='Resume';
-function myresume(){
-    try{
-    loader.innerHTML='<h2>Loading Data . . . </h2>'
-container.innerHTML= '<h2>My Resume<h2>'
-container.appendChild(h1);
-    }
-    catch(e){
-        loader.innerHTML='<h2 style=color:red>Error in loading data</h2>'
-    }
-    finally{
-        loader.innerHTML='';
-    }
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Resume';
 
 
+function myresume() {
+    try {
+        container.innerHTML = '<h2>My Resume</h2>';
+        container.appendChild(h1);
+    } catch (e) {
+        loader.innerHTML = '<h2 style="color:red">Error in loading data</h2>';
+        container.appendChild(loader);
+    }
 }
-button.addEventListener('click',ping);
+
+button.addEventListener('click', myresume);
